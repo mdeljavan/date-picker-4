02544a3e02978bd9032a73148334709fc9b72f5d
mdeljavan <mohammadrezadeljavan@gmail.com>
2018-03-30 23:09:50 +0430

    work on double click year

--- src/components/YearViews/YearViews.js
+++ src/components/YearViews/YearViews.js
@@ -3,22 +3,23 @@ import { checkCurrentPrevNext } from '../../utility/checkCurrentPrevNext';
 import JalaliDate from './../../lib/JalaliDate';
 import ListShow from '../../utility/List/List';
 class YearViews extends Component {
-  constructor ( props ) {
-    super( props );
-    const { min, max } = this.setMinAndMaxYear( props.min, props.max )
-    this.years = this.initializeYearValues( min, max );
+  constructor(props) {
+    super(props);
+    const { min, max } = this.setMinAndMaxYear(props.min, props.max)
+    this.years = this.initializeYearValues(min, max);
     this.state = {
-      current: this.years.find( val => {
+      current: this.years.find(val => {
         return val.value === this.props.current
-      } ).index-1
+      }).index - 1,
+      doubleClicked:false
     };
   };
-  setMinAndMaxYear = ( min, max ) => {
+  setMinAndMaxYear = (min, max) => {
     let _max = max, _min = min;
-    if ( !_min ) {
+    if (!_min) {
       _min = new JalaliDate().getFullYear() - 5;
     };
-    if ( !_max ) {
+    if (!_max) {
       _max = _min + 10;
     };
     return {
@@ -26,69 +27,76 @@ class YearViews extends Component {
       max: _max
     };
   };
-  initializeYearValues = ( min, max ) => {
+  initializeYearValues = (min, max) => {
     const years = [];
-    for ( let i = min, counter = 1; i <= max; i++ ) {
-      years.push( {
+    for (let i = min, counter = 1; i <= max; i++) {
+      years.push({
         index: counter,
         value: i
-      } );
+      });
       counter++;
     };
     return years;
   };
   year = () => {
-    return checkCurrentPrevNext( this.state.current, this.years.length - 1, 0 );
+    return checkCurrentPrevNext(this.state.current, this.years.length - 1, 0);
   };
-  onChangeHandler = ( val ) => {
-    this.setState( prevState => {
-      const current = checkCurrentPrevNext( prevState.current - val, this.years.length - 1, 0 ).current;
+  onChangeHandler = (val) => {
+    this.setState(prevState => {
+      const current = checkCurrentPrevNext(prevState.current - val, this.years.length - 1, 0).current;
       return {
         current
       }
     }
     );
   };
-  render () {
+  onDoubleClickHandler = () => {
+    this.setState(prevState=>{
+      return {
+        doubleClicked:!prevState.doubleClicked
+      };
+    });
+  };
+  render() {
     const { current, prev, next } = this.year();
-    const viewedYear = this.years.filter( ( val, ind ) => {
+    const viewedYear = this.years.filter((val, ind) => {
       return ind === current || ind === prev || ind === next;
-    } ).map( val => {
+    }).map(val => {
       let state = 'current';
-      if ( val.index - 1 === prev ) {
+      if (val.index - 1 === prev) {
         state = 'bfr';
-      } else if ( val.index - 1 === next ) {
+      } else if (val.index - 1 === next) {
         state = 'next'
       };
       return { ...val, state }
-    } );
+    });
     return (
       <ul
-        className="pick pick-y"
-        onDoubleClick={ this.onDoubleClickHandler }>
-        <ListShow list={ viewedYear } />
+        className={'pick pick-y '+ (this.state.doubleClicked? 'pick-jump': '')}
+        onDoubleClick={this.onDoubleClickHandler}>
+        <ListShow list={viewedYear} />
         <div
           className="pick-arw pick-arw-s1 pick-arw-r"
-          onClick={ () => this.onChangeHandler( 1 ) }>
+          onClick={() => this.onChangeHandler(1)}>
           <i
             className="fas fa-chevron-right pick-i-r"></i>
         </div>
         <div
           className="pick-arw pick-arw-s1 pick-arw-l"
-          onClick={ () => this.onChangeHandler( -1 ) }>
+          onClick={() => this.onChangeHandler(-1)}>
           <i
             className="fas fa-chevron-left pick-i-l">
           </i>
         </div>
         <div
           className="pick-arw pick-arw-s2 pick-arw-r"
-          onClick={ () => this.onChangeHandler( 1 ) }>
+          onClick={() => this.onChangeHandler(1)}>
           <i
             className="fas fa-chevron-right pick-i-r"></i>
         </div>
         <div
           className="pick-arw pick-arw-s2 pick-arw-l"
-          onClick={ () => this.onChangeHandler( -1 ) }>
+          onClick={() => this.onChangeHandler(-1)}>
           <i
             className="fas fa-chevron-left pick-i-l">
           </i>
